6a28d5008a6fc4fc575bc3716e9ece6a3c94217f
AayushParashar28 <ayushparashar6203@gmail.com>
2024-10-21 16:25:21 +0530

    Project Completed

--- src/route/expenseRoutes.js
+++ src/route/expenseRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const expenseController = require('../Controller/expensesController');
 
 router.post('/', expenseController.createExpense);
-router.get('/:userId', expenseController.getExpensesByUsersId);
+router.get('/:userId', expenseController.getExpensesByUserId);
 router.get('/', expenseController.getAllExpenses);
 router.get('/balance-sheet', expenseController.getBalanceSheet);
 
